0bfa039393b3e5ef192d79927b7124df3e2d9986
kkulikowski <kontakt@internetowakuznia.pl>
2017-01-15 22:00:04 +0100

    Add price

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,10 +5,13 @@
  * package com.example.android.justjava;
  */
 package com.example.android.justjava;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -25,7 +28,17 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(2);
+        displayPrice(2 * 5);
+    }
+
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
